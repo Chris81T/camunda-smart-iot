1edf7d9189336469a8bdc177341ae93d3dba85ba
Christian Thomas <christian@ckthomas.de>
2021-09-27 17:36:24 +0200

    started to fill a fresh pom.xml

---
+++ src/test/kotlin/de/ckthomas/smart/iot/camundasmartiot/CamundaSmartIotApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.ckthomas.smart.iot.camundasmartiot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CamundaSmartIotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
